7743135048f070dbcfa1bf0d6172330c36a09df9
Yu-Hung Lin <yuhung@yuhunglin.com>
2012-11-02 13:26:20 -0700

    [CLC-607] Simple user service endpoint

---
+++ lib/api/user.rb
@@ -0,0 +1,30 @@
+require 'json'
+require 'rest-client'
+
+module Api
+  class User < Sinatra::Base
+    root = "/api/user"
+
+    get "#{root}/status" do
+      if session[:user_id]
+        return {
+          :logged_in => true,
+          :uid => session[:user_id]
+        }.to_json
+      else
+        return {
+          :logged_in => false
+        }.to_json
+      end
+    end
+
+    get "#{root}/:uid" do
+      #eventually check db for existance of uid, etc.
+      return {
+        :uid => params[:uid],
+        :widget_data => {}
+      }.to_json
+    end
+
+  end
+end
