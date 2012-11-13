aba75406c61c2ed2c6893bbd9a4074612f90636c
Chris Tweney <chris@media.berkeley.edu>
2012-11-13 13:20:12 -0800

    CLC-652 Make the fakeable_proxy test fixtures plainly readable JSON, and suppress sensitive auth header data

--- lib/api/canvas.rb
+++ lib/api/canvas.rb
@@ -17,16 +17,20 @@ module Api
     # courses bound to some user's specific api key.
     get "#{root}/mycourses" do
       make_request "canvas/mycourses" do
-        RestClient.get("#{Settings.canvas_proxy.canvas_root}/api/v1/courses",
-                       {:Authorization => "Bearer #{Settings.canvas_proxy.admin_access_token}"})
+        RestClient.get("#{Settings.canvas_proxy.canvas_root}/api/v1/courses", {
+            :Authorization => "Bearer #{Settings.canvas_proxy.admin_access_token}",
+            :accept_encoding => ''
+        })
       end
     end
 
     # TODO clean up this duplication in the rails-api scheme
     get "/fake#{root}/mycourses" do
       make_request("canvas/mycourses", true) do
-        RestClient.get("#{Settings.canvas_proxy.canvas_root}/api/v1/courses",
-                       {:Authorization => "Bearer #{Settings.canvas_proxy.admin_access_token}"})
+        RestClient.get("#{Settings.canvas_proxy.canvas_root}/api/v1/courses", {
+            :Authorization => "Bearer #{Settings.canvas_proxy.admin_access_token}",
+            :accept_encoding => ''
+        })
       end
     end
 
--- lib/api/fakeable_proxy.rb
+++ lib/api/fakeable_proxy.rb
@@ -5,6 +5,11 @@ module Api
     c.hook_into :fakeweb
     c.allow_http_connections_when_no_cassette = true
     c.debug_logger = File.open(Rails.root.join("log", "vcr-debug.log"), 'w')
+    c.before_record do |i|
+      # so we don't record sensitive auth headers
+      i.response.headers['authorization'] = ''
+      i.request.headers['authorization'] = ''
+    end
   end
 
   class FakeableProxy < Sinatra::Base
@@ -16,7 +21,11 @@ module Api
     def make_request(request_id, force_fake = nil, &block)
       if fake || force_fake || Settings.freshen_vcr
         p "Using VCR to wrap request id #{request_id}"
-        VCR.use_cassette(request_id, :allow_playback_repeats => true, :match_requests_on => [:method, :path]) do
+        VCR.use_cassette(request_id,
+                         :allow_playback_repeats => true,
+                         :match_requests_on => [:method, :path],
+                         :serialize_with => :json,
+                         :preserve_exact_body_bytes => false) do
           block.call
         end
       else
